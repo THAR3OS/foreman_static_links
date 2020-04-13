f5277662649381f990417e3781d3945c67dee427
Seitz, S. (BNotK) <s.seitz@bnotk.de>
2020-04-13 21:45:48 +0200

    next try

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,25 @@
+require 'yaml'
+
+module ForemanStaticLinks
+  # Example: Plugin's HostsController inherits from Foreman's HostsController
+  class DashboardController < ::ApplicationController
+    # change layout if needed
+    # layout 'foreman_static_links/layouts/new_layout'
+
+    def static_links
+      begin
+        hash = YAML.load(File.read("/etc/foreman/foreman_static_links.yaml"))
+        @linklist = hash['links']
+      rescue
+        @linklist = [
+          {
+            :title       => 'foreman_static_links is currently unconfigured',
+            :description => 'please see the README.',
+            :url         => 'https://github.com/THAR3OS/foreman_static_links/',
+            :target      => '_blank'
+          },
+        ]
+      end
+    end
+  end
+end
