041e10d79d7c9aa28d2a45ab136196288c0d3b42
Seitz, S. (BNotK) <s.seitz@bnotk.de>
2020-04-13 14:57:49 +0200

    try to parse links from /etc/foreman/foreman_static_links.yaml

--- lib/foreman_static_links.rb
+++ lib/foreman_static_links.rb
@@ -1,4 +1,6 @@
+require 'yaml'
 require 'foreman_static_links/engine'
 
 module ForemanStaticLinks
+  links = YAML.load(File.read("/etc/foreman/foreman_static_links.yaml"))
 end
--- lib/foreman_static_links/version.rb
+++ lib/foreman_static_links/version.rb
@@ -1,3 +1,3 @@
 module ForemanStaticLinks
-  VERSION = '0.0.6'
+  VERSION = '0.0.7'
 end
