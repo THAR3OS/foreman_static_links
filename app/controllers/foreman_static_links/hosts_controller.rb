2ab672f5c204451954d25359767691f87953bd28
Seitz, S. (BNotK) <s.seitz@bnotk.de>
2020-04-13 15:21:53 +0200

    first trial to add real content

--- app/controllers/foreman_static_links/hosts_controller.rb
+++ app/controllers/foreman_static_links/hosts_controller.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 module ForemanStaticLinks
   # Example: Plugin's HostsController inherits from Foreman's HostsController
   class HostsController < ::HostsController
@@ -5,7 +7,18 @@ module ForemanStaticLinks
     # layout 'foreman_static_links/layouts/new_layout'
 
     def static_links
-      # automatically renders view/foreman_static_links/monitor#static_links
+      begin
+        @links = YAML.load(File.read("/etc/foreman/foreman_static_links.yaml"))
+      rescue
+        @links = [
+          {
+            title: 'foreman_static_links is currently unconfigured',
+            description: 'please see the README.',
+            link: 'https://github.com/THAR3OS/foreman_static_links/',
+            target: '_new'
+          },
+        ]
+      end
     end
   end
 end
--- lib/foreman_static_links.rb
+++ lib/foreman_static_links.rb
@@ -1,6 +1,4 @@
-require 'yaml'
 require 'foreman_static_links/engine'
 
 module ForemanStaticLinks
-  links = YAML.load(File.read("/etc/foreman/foreman_static_links.yaml"))
 end
